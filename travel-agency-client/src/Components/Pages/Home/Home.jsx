5b032571707ccd0dfc814d3b23156945653ea213
fl9mdasif <freelancer.mdasifofficial@gmail.com>
2023-04-10 15:28:42 +0600

    testimonial section complete

--- travel-agency-client/src/Components/Pages/Home/Home.jsx
+++ travel-agency-client/src/Components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../../HeroSection/Hero';
 import BusinessSummery from '../../BusinessSummery/BusinessSummery';
 import ExploreWorld from '../../ExploreWorld/ExploreWorld';
+import Reviews from '../../Reviews/Reviews';
 // import Hero from '../../HeroSection/Hero';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
             <Hero />
             <BusinessSummery />
             <ExploreWorld />
+            <Reviews />
         </>
     );
 };
---
+++ travel-agency-client/src/Components/Reviews/Reviews.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Reviews = () => {
+    return (
+        <div>
+            <section class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap -m-4">
+                        <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRR6VpQ-2pOW_Pl9gXkTqa2pqSkGVrXfVwF0HnZ8IzlMhJIIgiFksxH7136-KHb-99EthA&usqp=CAU" />
+                                <p class="leading-relaxed">This travel agency website made planning my vacation a breeze. The user-friendly interface, unbeatable prices, and helpful customer service team exceeded my expectations. Highly recommend!</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HOLDEN CAULFIELD</h2>
+                                <p class="text-gray-500">Senior Product Designer</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/3 lg:mb-0 mb-6 p-4">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpIoI76wjvQ2pq6t25TLmRAwTxv118OFKZxykoCiY_fWRtR8QRr1nWWcDtG3tHdgCuWu8&usqp=CAU" />
+                                <p class="leading-relaxed">The best travel agency website out there! The team provided me with personalized recommendations, great deals, and made my trip planning effortless. Highly recommend to anyone in need of travel assistance</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">ALPER KAMU</h2>
+                                <p class="text-gray-500">UI Develeoper</p>
+                            </div>
+                        </div>
+                        <div class="lg:w-1/3 lg:mb-0 p-4">
+                            <div class="h-full text-center">
+                                <img alt="testimonial" class="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80" />
+                                <p class="leading-relaxed">I was blown away by the level of service and attention to detail provided by this travel agency website. They helped me plan a dream trip to Europe with ease. I will definitely use them again!"</p>
+                                <span class="inline-block h-1 w-10 rounded bg-indigo-500 mt-6 mb-4"></span>
+                                <h2 class="text-gray-900 font-medium title-font tracking-wider text-sm">HENRY LETHAM</h2>
+                                <p class="text-gray-500">CTO</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Reviews;
